cf6d94b86874a77e9cc9d3c55465651fdefaaadc
Eric Sipple <esipple@stitchfix.com>
2017-01-31 21:01:55 -0800

    Changing table drinks to glasses.

--- db/migrate/20170201014154_add_glasses.rb
+++ db/migrate/20170201014154_add_glasses.rb
@@ -1,7 +1,7 @@
-class AddDrinks < ActiveRecord::Migration[5.0]
+class AddGlasses < ActiveRecord::Migration[5.0]
   def change
 
-    create_table :drinks do |t|
+    create_table :glasses do |t|
       t.integer :wine_id, null: false
       t.integer :user_id, null: false
       t.date :date, null: false
