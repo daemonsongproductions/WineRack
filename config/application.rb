95dc9731b315381137f7c166917a457f53cca67a
Eric Sipple <esipple@stitchfix.com>
2017-05-27 14:13:52 -0400

    Installing webpacker.

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module Winerack
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Eastern Time (US & Canada)'
-    config.active_record.raise_in_transactional_callbacks = true
     config.active_record.schema_format = :sql
     config.autoload_paths += %W(#{config.root}/app/services)
   end
